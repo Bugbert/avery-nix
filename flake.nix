658e4c56e318ddfe97da1ea87d5e098e729412d5
Avery Andrews <avery.andrews@mailfence.com>
2024-05-06 23:12:27 -0400

    yoo

--- flake.nix
+++ flake.nix
@@ -13,11 +13,11 @@
         system = "x86_64-linux";
 	modules = [
 	  ./configuration.nix
-          home-manager.nixosModules.home-manager
-	  {
-	    home-manager.useGlobalPkgs = true;
-	    home-manager.useUserPackages = true;
-	    home-manager.users.avery = import ./home.nix;
+	  home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.avery = import ./home.nix;
 	  }
 	];
       };
